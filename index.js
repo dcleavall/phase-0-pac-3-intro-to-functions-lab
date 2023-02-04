37af8dd16a39f1aa4df46a4e2f217197306e4743
Derrick <dcleavall@gmail.com>
2023-02-03 17:06:35 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    return string .toUpperCase();
+}
+function whisper(string) {
+    return string .toLowerCase();
+}
+function logShout(string) {
+    console.log('HELLO');
+}
+function logWhisper(string) {
+    console.log('hello');
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string=== string.toLowerCase()) {
+    return string= "I can't hear you!"
+  } else if (string=== string.toUpperCase()) {
+    return string= "YES INDEED!"
+  } else (string=== "Let's have dinner together!") 
+  return string= "I would love to!" 
+}
+
+
+
+
+
+ 
+    
+  
+
+
+
+    
+
+    
+
+
